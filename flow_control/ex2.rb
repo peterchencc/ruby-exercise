2d0f3cfbb7f27d6f57fe80a61ae21c4c79852f74
peterchencc <peterchencc@gmail.com>
2015-03-23 17:28:31 +0800

    flow_control ex2 submit

--- flow_control/ex2.rb
+++ flow_control/ex2.rb
@@ -3,3 +3,15 @@ b=a.upcase!
 puts a
 puts b
 puts "lol".upcase
+
+def caps(string)
+  if string.length>10
+    string.upcase
+  else
+    string
+  end
+
+end
+
+puts caps("Peter")
+puts caps("i'm in alphacamp")
