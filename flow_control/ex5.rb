4e614c6dccc83f709e300cbfcfc51bd10aacb378
peterchencc <peterchencc@gmail.com>
2015-03-23 18:53:30 +0800

    flow_control ex5 submit

--- flow_control/ex5.rb
+++ flow_control/ex5.rb
@@ -10,6 +10,37 @@ def checknum(number)
   end
 end
 
+def case1(number)
+  case
+  when number<0
+    puts "number<0"
+  when number <= 50
+    puts "#{number} is between 0 and 50"
+  when number <= 100
+    puts "#{number} is between 51 and 100"
+  else
+    puts "#{number} is above 100"
+  end
+end
+
+def case2(num)
+  case num
+  when 0..50
+    puts "#{num} is between 0 and 50"
+  when 51..100
+    puts "#{num} is between 51 and 100"
+  else
+    if num < 0
+      puts "You can't enter a negative num!"
+    else
+      puts "#{num} is above 100"
+    end
+  end
+end
 puts "enter a number"
 a= gets.chomp.to_i
 checknum(a)
+case1(a)
+case2(a)
+
+
