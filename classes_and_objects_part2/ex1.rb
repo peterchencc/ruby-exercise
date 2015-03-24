c54a165983e1e5c2e7e2b36c18096d0dcf85f936
peterchencc <peterchencc@gmail.com>
2015-03-24 17:31:44 +0800

    classes_and_objects_part2 ex1 submit

---
+++ classes_and_objects_part2/ex1.rb
@@ -0,0 +1,27 @@
+class MyCar
+  attr_accessor :color
+  attr_reader :year, :model
+
+  def initialize(year, model, color)
+    @year =year
+    @color =color
+    @model = model
+
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "new color is #{color}"
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+end
+
+# chelsea=MyCar.new(1997,'dqwdq','blue')
+
+MyCar.gas_mileage(5,230)
+
+
+
