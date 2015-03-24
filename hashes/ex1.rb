3fc621d96182036643184cd2e821489c2f375812
peterchencc <peterchencc@gmail.com>
2015-03-24 15:14:14 +0800

    hashes ex1 submit

---
+++ hashes/ex1.rb
@@ -0,0 +1,12 @@
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+
+
+          immediate=family.select do |k,v|
+            k==:sisters || k == :brothers
+          end
+
+          p immediate.values.flatten
